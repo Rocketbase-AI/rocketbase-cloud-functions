d793d8343c143084592204d6b6ba6c0497654beb
Heiki Riesenkampf <hr1514@ic.ac.uk>
2019-04-16 15:02:38 +0200

    Add new cloud functions for credentials and adding new models

--- src/functions/src/constants.ts
+++ src/functions/src/constants.ts
@@ -1,3 +1,8 @@
 export const DEFAULT_REGION: string = "europe-west1";
 export const HOSTING_REGION: string = "us-central1";
-export const USERS_COLLECTION_NAME: string = "userProfiles";
+export const USERS_COLLECTION_NAME: string = "users";
+export const MODELS_COLLECTION_NAME: string = "models";
+export const MODELS_SELECTION_EVENT: string = "SELECT_MODEL";
+export const GET_CREDENTIALS_EVENT: string = "GET_CREDENTIALS";
+export const MODEL_SAVE_EVENT: string = "SAVE_MODEL";
+export const PYTORCH_FRAMEWORK: string = "PyTorch";
--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -2,13 +2,29 @@ import * as path from "path";
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as next from "next";
-import {} from "./constants";
+import * as cors from "cors";
+import * as mixpanel from "mixpanel";
+import {
+  DEFAULT_REGION,
+  MODELS_COLLECTION_NAME,
+  MODELS_SELECTION_EVENT,
+  MODEL_SAVE_EVENT,
+  PYTORCH_FRAMEWORK,
+  GET_CREDENTIALS_EVENT,
+} from "./constants";
+import credentials from "./credentials";
 
 // Initializes Cloud Functions.
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 
 // Firestore settings.
-// const db = admin.firestore();
+const db = admin.firestore();
+
+// Initiate Mixpanel client
+const Mixpanel = mixpanel.init("793aeec70db39b86d15260f129ec4680");
+
+// CORS configuration.
+const corsHandler = cors({ origin: true });
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({
@@ -21,22 +37,97 @@ export const nextApp = functions.https.onRequest((req, res) => {
   return app.prepare().then(() => handle(req, res));
 });
 
-// exports.createInitialUserProfile = functions
-//   .region(DEFAULT_REGION)
-//   .auth.user()
-//   .onCreate(async user => {
-//     const initialUserData = {
-//       created_at: new Date(),
-//       last_login: new Date(),
-//       admin: false,
-//     };
-//     try {
-//       const writeResult = await db
-//         .collection(USERS_COLLECTION_NAME)
-//         .doc(user.uid)
-//         .set(initialUserData);
-//       return writeResult.writeTime.toDate();
-//     } catch (error) {
-//       return error;
-//     }
-//   });
+export const getAvailableModels = functions
+  .region(DEFAULT_REGION)
+  .https.onRequest(async (req, res) => {
+    corsHandler(req, res, () => {
+      // allow cross-domain calls from any origin
+    });
+    if (req.method !== "GET") {
+      return res.status(405).send(`${req.method} method not allowed.`);
+    }
+    const { author, model, version } = req.query;
+
+    if (!model || !author) {
+      return res.status(400).send("Required request parameters missing.");
+    }
+    Mixpanel.track(MODELS_SELECTION_EVENT, { author, model, version });
+    let modelSnapShot;
+    try {
+      modelSnapShot = await db
+        .collection(MODELS_COLLECTION_NAME)
+        .where("author", "==", author)
+        .where("slug", "==", model)
+        .get();
+    } catch (e) {
+      return res.status(500).send(`Internal database error.`);
+    }
+    const allModels: any[] = modelSnapShot.docs.map(doc => doc.data());
+    return res.status(200).json(allModels);
+  });
+
+export const getUploadCredentials = functions
+  .region(DEFAULT_REGION)
+  .https.onRequest(async (req, res) => {
+    corsHandler(req, res, () => {
+      // allow cross-domain calls from any origin
+    });
+    if (req.method !== "GET") {
+      return res.status(405).send(`${req.method} method not allowed.`);
+    }
+    const { token } = req.query;
+
+    if (!token) {
+      return res.status(400).send("Required request parameters missing.");
+    }
+    Mixpanel.track(GET_CREDENTIALS_EVENT, { token });
+    return res.status(200).json(credentials);
+  });
+
+export const saveNewModel = functions
+  .region(DEFAULT_REGION)
+  .https.onRequest(async (req, res) => {
+    corsHandler(req, res, () => {
+      // allow cross-domain calls from any origin
+    });
+    if (req.method !== "POST") {
+      return res.status(405).send(`${req.method} method not allowed.`);
+    }
+    const {
+      author,
+      model,
+      version,
+      folderName,
+      modelFilePath,
+      isPrivate,
+    } = req.body;
+
+    if (
+      !model ||
+      !author ||
+      !version ||
+      !folderName ||
+      !modelFilePath ||
+      !isPrivate
+    ) {
+      return res.status(400).send("Required request parameters missing.");
+    }
+    Mixpanel.track(MODEL_SAVE_EVENT, { author, model, version, isPrivate });
+    const newModel = {
+      author,
+      folderName,
+      framework: PYTORCH_FRAMEWORK,
+      model,
+      modelFilePath,
+      private: isPrivate,
+      version,
+      publishedDate: admin.firestore.Timestamp.fromDate(new Date()),
+    };
+    let modelRef;
+    try {
+      modelRef = await db.collection(MODELS_COLLECTION_NAME).add(newModel);
+    } catch (e) {
+      return res.status(500).send(`Internal database error.`);
+    }
+    return res.status(201).json({ id: modelRef.id });
+  });
